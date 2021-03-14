7ede1e3500fec0454e02e66ccb41b977eb5c4829
Tholhuijsen, P.T.C.M <p.t.c.m.tholhuijsen@student.tue.nl>
2021-03-14 19:55:03 +0100

    Fixes for coloured nonograms

--- nonogen/generator.py
+++ nonogen/generator.py
@@ -16,6 +16,10 @@ class no_generator:
         self._pixels = None
         self._m = 32
 
+    @staticmethod
+    def r_g_b_to_hx(rgb: set) -> str:
+        return "#%02x%02x%02x" % rgb
+
     def run(self):
         try:
             self._resize()
@@ -23,8 +27,13 @@ class no_generator:
                 self._convert()
 
             self._pixel_matrix()
-            self._visualize_grid()
+
+            img = self._new_image()
+            self._solution = img
+
             self._fill_solution_grid()
+            self._solution = self._visualize_grid(self._solution)
+
             # self._nonogram = self._img
 
             self._save_solution()
@@ -43,53 +52,57 @@ class no_generator:
     def _convert(self):
         self._img = self._img.convert("1")
 
-    def _visualize_grid(self):
-        img = Image.new(mode="L", size=((int(self._size[0]) + 2) * self._m,
-                                        (int(self._size[1]) + 2) * self._m), color=255)
+    def _new_image(self) -> Image:
+        img = Image.new(mode="RGB", size=((int(self._size[0]) + 2) * self._m,
+                                          (int(self._size[1]) + 2) * self._m), color=(255, 255, 255))
 
-        draw = ImageDraw.Draw(img)
+        return img
+
+    def _visualize_grid(self, img: Image) -> Image:
+        draw = ImageDraw.Draw(img, mode="RGB")
+
         counter = 0
         for x in range(self._m, img.width - self._m + 1, self._m):
             if counter % 5 == 0:
-                draw.line(((x, self._m), (x, img.width - self._m)), width=2, fill=0)
+                draw.line(((x, self._m), (x, img.width - self._m)), width=2, fill=(0, 0, 0))
             elif counter == int(self._size[0]):
-                draw.line(((x, self._m), (x, img.width - self._m)), width=2, fill=0)
+                draw.line(((x, self._m), (x, img.width - self._m)), width=2, fill=(0, 0, 0))
             else:
-                draw.line(((x, self._m), (x, img.width - self._m)), fill=0)
+                draw.line(((x, self._m), (x, img.width - self._m)), fill=(0, 0, 0))
 
             counter += 1
 
         counter = 0
         for y in range(self._m, img.height - self._m + 1, self._m):
             if counter % 5 == 0:
-                draw.line(((self._m, y), (img.height - self._m, y)), width=2, fill=0)
+                draw.line(((self._m, y), (img.height - self._m, y)), width=2, fill=(0, 0, 0))
             elif counter == int(self._size[1]):
-                draw.line(((self._m, y), (img.height - self._m, y)), width=2, fill=0)
+                draw.line(((self._m, y), (img.height - self._m, y)), width=2, fill=(0, 0, 0))
             else:
-                draw.line(((self._m, y), (img.height - self._m, y)), fill=0)
+                draw.line(((self._m, y), (img.height - self._m, y)), fill=(0, 0, 0))
 
             counter += 1
 
         del counter
         del draw
 
-        self._solution = img
+        return img
 
     def _fill_solution_grid(self):
-        draw = ImageDraw.Draw(self._solution)
+        draw = ImageDraw.Draw(self._solution, mode="RGB")
         for row_index in range(len(self._pixels)):
             for column_index in range(len(self._pixels[row_index])):
-                if self._pixels[row_index][column_index] != 255:
+                if self._pixels[row_index][column_index] != 255 and self._pixels[row_index][column_index] != (255, 255, 255):
                     if self._colour:
                         draw.rectangle([(self._m + column_index * self._m, self._m + row_index * self._m),
                                         (self._m + column_index * self._m + self._m,
                                          self._m + row_index * self._m + self._m)],
-                                       fill=int(statistics.mean(self._pixels[row_index][column_index])))
+                                       fill=self._pixels[row_index][column_index])
                     else:
                         draw.rectangle([(self._m + column_index * self._m, self._m + row_index * self._m),
                                         (self._m + column_index * self._m + self._m,
                                          self._m + row_index * self._m + self._m)],
-                                       fill=0)
+                                       fill=(0, 0, 0))
         del draw
 
     def _save_nonogram(self):
